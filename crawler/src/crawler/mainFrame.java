c5faaa8e05a9b4751a1591bf1ec33fe6e837e97b
wow101 <wow101@yopmail.com>
2014-06-27 21:48:09 +0900

    gui구성

--- crawler/src/crawler/mainFrame.java
+++ crawler/src/crawler/mainFrame.java
@@ -6,6 +6,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTree;
+import javax.swing.JList;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import javax.swing.AbstractListModel;
+import javax.swing.JPasswordField;
+import javax.swing.SpringLayout;
+import java.awt.Component;
+import javax.swing.JSplitPane;
+import java.awt.Dimension;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 public class mainFrame extends JFrame {
 
@@ -32,11 +44,37 @@ public class mainFrame extends JFrame {
 	 */
 	public mainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 740, 686);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JSplitPane splitPane = new JSplitPane();
+		contentPane.add(splitPane);
+		
+		JTree tree = new JTree();
+		tree.setModel(new DefaultTreeModel(
+			new DefaultMutableTreeNode("JTree") {
+				{
+				}
+			}
+		));
+		tree.setMinimumSize(new Dimension(400, 0));
+		tree.setMaximumSize(new Dimension(0, 0));
+		splitPane.setLeftComponent(tree);
+		
+		JList list = new JList();
+		list.setFixedCellWidth(200);
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		splitPane.setRightComponent(list);
 	}
-
 }
